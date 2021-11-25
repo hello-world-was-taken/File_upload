d59b1e19bb3b30dc9a2064ee415718772f3ec613
hello_world <haileformal@gmail.com>
2021-11-25 11:17:05 +0300

    configure app level view

--- upload_app/views.py
+++ upload_app/views.py
@@ -1,3 +1,10 @@
+from django.db import models
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import UserUpload
 # Create your views here.
+
+class UploadListView(ListView):
+    model = UserUpload
+    template_name = 'home.html'
+    context_object_name = 'upload'
