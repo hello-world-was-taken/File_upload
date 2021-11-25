979e9cbd4396767326d25c4120a0932ce4a2c7d9
hello_world <haileformal@gmail.com>
2021-11-25 10:08:13 +0300

    create model for uploads

--- upload_app/models.py
+++ upload_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class UserUpload(models.Model):
+    ass_name = models.CharField(max_length=30)
+    img = models.ImageField(upload_to='images/')
+    file = models.FileField(upload_to='files/')
+
+    def __str__(self):
+        return self.ass_name
